5110c88aabba04f9b36243610cc8dd6c905bdc16
Maxim <maximorechov@yandex.ru>
2021-02-13 16:43:40 +0500

    Field tests for the Point added

--- src/io/baik/xo/model/Field.java
+++ src/io/baik/xo/model/Field.java
@@ -1,10 +1,17 @@
 package io.baik.xo.model;
 
+import io.baik.xo.model.exeptions.AlreadyOccupiedException;
+import io.baik.xo.model.exeptions.InvalidPointException;
+
 import java.awt.*;
 
 public class Field {
 
     private final int INT_SIZE = 3;
+
+    private final int MIN_COORDINATE = 0;
+
+    private final int MAX_COORDINATE = INT_SIZE;
 
     private final Figure[][] field = new Figure[INT_SIZE][INT_SIZE];
     
@@ -12,11 +19,30 @@ public class Field {
         return INT_SIZE;
     }
 
-    public Figure getFigure(Point point) {
+    public Figure getFigure(Point point) throws InvalidPointException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, Figure figure) {
+    public void setFigure(final Point point, Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+
+        if (field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
