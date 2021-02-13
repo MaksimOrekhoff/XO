9ce88923d6064f69c5d1b5b2d5e9da8f4d1ed7f5
Maxim <maximorechov@yandex.ru>
2021-02-13 17:07:28 +0500

    FieldTests check for the exceptions have been added

--- test/io/baik/xo/model/FieldTest.java
+++ test/io/baik/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.baik.xo.model;
 
+import io.baik.xo.model.exeptions.AlreadyOccupiedException;
 import io.baik.xo.model.exeptions.InvalidPointException;
 import org.junit.Test;
 
@@ -88,6 +89,24 @@ public class FieldTest {
 
         }
 
+    }
+
+    @Test
+    public void testSetFigureWhenAlreadyOccupied() throws Exception {
+
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+        field.setFigure(inputPoint, inputFigure);
+        final Figure actualFigure = field.getFigure(inputPoint);
+        assertEquals(inputFigure, actualFigure);
+
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e) {
+
+        }
     }
 
 
