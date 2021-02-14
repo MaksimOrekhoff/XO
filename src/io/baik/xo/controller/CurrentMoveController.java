885a2c4dd0bc05e8c107434190928915f8ef3dba
Maxim <maximorechov@yandex.ru>
2021-02-14 18:45:34 +0500

    CurrentMoveController implemen

---
+++ src/io/baik/xo/controller/CurrentMoveController.java
@@ -0,0 +1,36 @@
+package io.baik.xo.controller;
+
+import io.baik.xo.model.Field;
+import io.baik.xo.model.Figure;
+import io.baik.xo.model.exeptions.InvalidPointException;
+
+import java.awt.*;
+
+public class CurrentMoveController {
+    public Figure currentMove(final Field field) throws InvalidPointException {
+        int countFigure = 0;
+        for (int x = 0; x < field.getSize(); x++) {
+            countFigure += countFiguresInTheRow(field, x);
+        }
+
+        if (countFigure == field.getSize() * field.getSize()) {
+            return null;
+        }
+
+        if (countFigure % 2 == 0) {
+            return Figure.X;
+        }
+
+        return Figure.O;
+    }
+
+    private int countFiguresInTheRow(final Field field, final int row) throws InvalidPointException {
+        int countFigure = 0;
+        for (int x = 0; x < field.getSize(); x++) {
+            if (field.getFigure(new Point(x, row)) != null) {
+                countFigure++;
+            }
+        }
+        return countFigure;
+    }
+}
